52208bcb13812a9c89bedd74321584591b0c2d3b
webcrawlr <raymondwmuller2@gmail.com>
2018-05-31 11:44:53 -0700

    Update postProcess.php

--- postProcess.php
+++ postProcess.php
@@ -44,7 +44,7 @@ if(isset($_POST['submitPost'])){
         require_once 'HTTP/Request2.php';
 
         //add the user's data to the http2 post request
-        $request=new HTTP_Request2('https://tailhub.herokuapp.com/users/post', HTTP_Request2::METHOD_POST);
+        $request=new HTTP_Request2('https://tailhub.herokuapp.com/users/newPost', HTTP_Request2::METHOD_POST);
 
         $request->addPostParameter('username',  "obiWanKenobi");  //Add username post-creation
 
