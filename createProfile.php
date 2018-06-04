c36b3825aa7757053da62a2de7756105101ca87b
djaydenk <djaydenk@gmail.com>
2018-06-03 19:26:19 -0700

    Update createProfile.php

--- createProfile.php
+++ createProfile.php
@@ -1,11 +1,12 @@
 <?php
+require_once 'HTTP/Request2.php';
 
 if(isset($_POST['Register'])){
 	$firstname = $_SESSION['firstname'];
 	$lastname = $_SESSION['lastname']
-    $username = $_SESSION['email'];
-    $password = $_SESSION['password'];
-    $month = $_SESSION['month'];
+    	$username = $_SESSION['email'];
+    	$password = $_SESSION['password'];
+    	$month = $_SESSION['month'];
 	$day = $_SESSION['day'];
 	$year = $_SESSION['year'];
 	$gender = $_SESSION['gender'];
@@ -15,7 +16,7 @@ if(isset($_POST['Register'])){
     $request=new HTTP_Request2('https://tailhub.herokuapp.com/createProfile', HTTP_Request2::METHOD_POST);
     $request->addPostParameter('fname', $firstname);
     $request->addPostParameter('lname', $lastname);
-	$request->addPostParameter('email', $email);
+    $request->addPostParameter('email', $email);
     $request->addPostParameter('password', $password);
     $request->addPostParameter('month', $month);
     $request->addPostParameter('day', $day);
